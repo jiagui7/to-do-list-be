b7b551b87a9c2aed3c68d0db9f863427b9522140
fran <franjimenezaguilera@gmail.com>
2021-06-13 17:36:20 +0200

    Add post request

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require("express");
-var app = express();
 var cors = require("cors");
 var db = require("./database");
+var bodyParser = require("body-parser");
 
 var HTTP_PORT = 8080;
 
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/task", (req, res, next) => {
@@ -14,6 +17,20 @@ app.get("/task", (req, res, next) => {
     }
     res.status(200).json(rows);
   });
+});
+
+app.post("/task", (req, res, next) => {
+  let body = req.body;
+  db.run(
+    "INSERT INTO task (description, completed) VALUES (?,?)",
+    [body.data.description, false],
+    (err) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      }
+      res.status(200).json(true);
+    }
+  );
 });
 
 app.listen(HTTP_PORT, () => {
