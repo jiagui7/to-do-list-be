517cb7c5c076f0586f7e222d0ccee553e97263a9
fran <franjimenezaguilera@gmail.com>
2021-06-15 08:09:26 +0200

    Add put request route

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # to-do-list-be
+
+This is the backend for the To-Do List project.
+
+It is consumed by [To-Do List Frontend](https://github.com/jiagui7/to-do-list-fe) client.
+
+# Requeriments
+
+- NodeJS
+- npm
+
+# Dependencies
+
+Install the dependencies from the `package.json`. From the root of the project, enter in the terminal:
+
+```bash
+    npm install
+```  
+
+# Run
+
+To run in local, go to the root of the project and enter following command in the terminal:
+
+```bash
+    npm start
+```
+
+# Run tests
+
+To run the unit tests, from the root, enter in the terminal:
+
+```bash
+    npm test
+```
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var db = require("./database");
 var bodyParser = require("body-parser");
 
 var HTTP_PORT = 8080;
+var EMPTY_DESCRIPTION_ERROR = "Description cannot be empty";
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,10 +21,28 @@ app.get("/task", (req, res, next) => {
 });
 
 app.post("/task", (req, res, next) => {
-  let body = req.body;
+  let description = req.body.data.description;
+  if (description.length <= 0) {
+    res.status(400).json({ error: EMPTY_DESCRIPTION_ERROR });
+  }
   db.run(
     "INSERT INTO task (description, completed) VALUES (?,?)",
-    [body.data.description, false],
+    [description, false],
+    (err) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      }
+      res.status(200).json(true);
+    }
+  );
+});
+
+app.patch("/task/:id", (req, res, next) => {
+  let id = req.params.id;
+  let body = req.body;
+  db.run(
+    `UPDATE task SET completed = ? WHERE id = ?`,
+    [body.data.completed, id],
     (err) => {
       if (err) {
         res.status(400).json({ error: err.message });
@@ -36,3 +55,5 @@ app.post("/task", (req, res, next) => {
 app.listen(HTTP_PORT, () => {
   console.log(`Server running on port ${HTTP_PORT}...`);
 });
+
+module.exports = app;
